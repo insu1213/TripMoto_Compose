3ceda2f18d7a3d40c7f1e95ec5d5a0559d6b9a52
insu1213 <32616560+insu1213@users.noreply.github.com>
2023-08-28 23:44:06 +0900

    sync: 20230828-02

--- app/src/main/java/com/insu/tripmoto_compose/model/service/FCMService.kt
+++ app/src/main/java/com/insu/tripmoto_compose/model/service/FCMService.kt
@@ -1,7 +1,11 @@
 package com.insu.tripmoto_compose.model.service
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -11,6 +15,43 @@ import com.google.firebase.messaging.ktx.remoteMessage
 import com.insu.tripmoto_compose.R
 
 class FCMService: FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // 알림 채널 생성
+        createNotificationChannel()
+
+        // 푸시 알림 생성
+        val notificationBuilder = NotificationCompat.Builder(this, "channel_id")
+            .setContentTitle(remoteMessage.notification?.title)
+            .setContentText(remoteMessage.notification?.body)
+            .setSmallIcon(R.drawable.google)
+            .setAutoCancel(true)
+
+        // 알림 표시
+        val notificationManager = NotificationManagerCompat.from(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "channel_id"
+            val channelName = "My Channel"
+            val channelDescription = "Description of the channel"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 //    override fun onMessageReceived(message: RemoteMessage) {
 //        super.onMessageReceived(message)
 //
@@ -22,9 +63,9 @@ class FCMService: FirebaseMessagingService() {
 //        }
 //    }
 
-    override fun onNewToken(token: String) {
-        super.onNewToken(token)
-    }
+//    override fun onNewToken(token: String) {
+//        super.onNewToken(token)
+//    }
 
 //    private fun showBackgroundNotification(title: String, body: String) {
 //        val notificationManager = NotificationManagerCompat.from(this)
